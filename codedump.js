c088fcd1deb26530f171346c90bc5fa3f7b2af53
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-07 15:14:41 +0000

    automatically backed up by learn

---
+++ codedump.js
@@ -0,0 +1,36 @@
+function scuberGreetingForFeet(distance) {
+  // console.log("== scuberGreetingForFeet ==");
+  if (distance <= 400) {
+    result = "This one is on me!"
+  } else if ((distance > 2000) && (distance <= 2500)) {
+    result = "I will gladly take your thirty bucks."
+  } else if (distance > 2500) {
+    result = "No can do."
+  }
+  // return result;
+}
+
+function ternaryCheckCity(city) {
+  // console.log("== ternaryCheckCity ==");
+  city === "NYC"
+    ? return result = "Ok, sounds good."
+    : return result = "No go."
+}
+
+function switchOnCharmFromTip(tip) {
+  // console.log("== switchOnCharmFromTip ==");
+  switch(tip) {
+      case "generous":
+          result = "Thank you so much."
+          break;
+      case !"generous":
+          result = "Thank you."
+          break;
+      default:
+          result = "Bye."
+  }
+}
+
+scuberGreetingForFeet(200);
+ternaryCheckCity("NYC");
+switchOnCharmFromTip("generous");
